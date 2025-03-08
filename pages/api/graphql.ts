9a0b41b68fe8b08ea0c5324ca666aa5857e18d6c
Arnat Ngawsuwan <annsw.work@gmail.com>
2025-03-08 15:07:42 +0700

    Remove Apollo Client initialization from GraphQL API handler

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -2,17 +2,6 @@ import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { typeDefs } from "@/graphql/schema";
 import { resolvers } from "@/graphql/resolvers";
-import { ApolloClient, InMemoryCache, HttpLink, NormalizedCacheObject } from "@apollo/client";
-
-
-// Initialize Apollo Client for querying external Pokémon API
-const client = new ApolloClient<NormalizedCacheObject>({
-  link: new HttpLink({
-    uri: "https://graphql-pokemon2.vercel.app/", // Pokémon GraphQL API
-    fetch,
-  }),
-  cache: new InMemoryCache(),
-});
 
 // Create Apollo Server
 const apolloServer = new ApolloServer({
